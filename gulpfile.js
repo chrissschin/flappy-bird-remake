6e3cbc42469ec63bd48e003ba3e30f0399319dda
chrissschin <solischris92@gmail.com>
2015-11-03 23:02:47 -0800

    added more gulp files

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,37 @@
 var gulp = require('gulp');
 
+var jshint = require('gulp-jshint');
 var browserify = require('browserify');
+var minifyHTML = require('gulp-minify-html');
+var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
 
+//js linting
+gulp.task('jshint', function() {
+  return gulp.src('site/js/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+
+//minify html
+gulp.task('minifyhtml', function(){
+  return gulp.src('index.html')
+  .pipe(minifyHTML())
+  .pipe(gulp.dest('build/'))
+});
+
+// sass
+gulp.task('sass', function(){
+  return gulp.src('scss/*.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('build/css'));
+});
+
+//uglify scripts combines them
 gulp.task('scripts', function() {
   return browserify(['js/main.js'])
   .bundle()
@@ -17,4 +43,4 @@ gulp.task('scripts', function() {
 
 
 // Build task
-gulp.task('build', ['scripts']);
+gulp.task('build', ['jshint','minifyhtml','scripts', 'sass']);
