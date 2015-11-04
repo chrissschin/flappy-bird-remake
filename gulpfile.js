10e7e215bb149f993d74728c487b375efb626358
chrissschin <solischris92@gmail.com>
2015-11-03 22:41:18 -0800

    gulp file works now

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 var gulp = require('gulp');
 
 var browserify = require('browserify');
+var uglify = require('gulp-uglify');
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
 
 
-gulp.task('scripts', function(){
-  return browserify('./js/*.js')
+gulp.task('scripts', function() {
+  return browserify(['js/main.js'])
   .bundle()
   .pipe(source('main.js'))
   .pipe(buffer())
+  .pipe(uglify())
   .pipe(gulp.dest('build/js'));
 });
 
